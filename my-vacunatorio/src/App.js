a87fb7f76bc24c753f5b7ad7891507ab33d2b25b
AnyelinaParedes <apag15194@hotmail.com>
2022-10-01 16:21:41 -0300

    cambios

---
+++ my-vacunatorio/src/App.js
@@ -0,0 +1,24 @@
+
+import Navbar from './componentes/Navbar';
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+     
+   
+      </div>
+  
+      
+      
+     
+   
+  );
+}
+
+export default App;
